ed4123d88ca60f1e976a53e58ff65eace58b4604
Jhony Avella <jhony.avella@edunext.co>
2021-11-23 13:56:14 -0500

    feat: adding a new card component that servers as a container/wrapper of other components (composition/children props)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import Card from './Card';
+import './Expenses.css';
 
 function Expenses (props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((value, index) => {
         return <ExpenseItem {...value} />
       })}
-    </div>
+    </Card>
   )
 }
 
